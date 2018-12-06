9efb852b4f00f63882df13aaf83aa4c085dd59f4
Eugene <q010@bk.ru>
2018-12-06 23:28:26 +0300

    creating devise views and configurating user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  def profile
+
+  end
+
+  def after_sign_in_path_for(resource)
+    current_user_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :technologies
   has_and_belongs_to_many :technologies
