5ac9a632df56eeb40ca7a96b17f6d132bb9af48a
Eugene <q010@bk.ru>
2018-12-12 21:03:25 +0300

    setting avatar for user and icon for tech

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def profile
     @user = current_user
   end
+<<<<<<< HEAD
+=======
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :lang, :avatar)
+  end
+
+>>>>>>> setting avatar for user and icon for tech
 end
--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,4 +1,9 @@
 class Technology < ApplicationRecord
   has_many :tests
   has_and_belongs_to_many :users
+<<<<<<< HEAD
+=======
+  has_one_attached :icon
+
+>>>>>>> setting avatar for user and icon for tech
 end
