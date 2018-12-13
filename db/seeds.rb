bebdad6e1b11e555991aab39ba3e343d17baf714
Eugene <q010@bk.ru>
2018-12-13 20:03:42 +0300

    adding users in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' },
 # { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Users = User.create([{ :username => 'fcbb99', :password => '123456', :email => 'fcbb99@mail.ru', :confirmed_at => '2018-12-31 15:32:10' },
+                     { :username => 'fcbb98', :password => '123456', :email => 'fcbb98@mail.ru', :confirmed_at => '2018-12-31 15:32:10' },
+                     { :username => 'fcbb97', :password => '123456', :email => 'fcbb97@mail.ru' },
+                     { :username => 'fcbb96', :password => '123456', :email => 'fcbb96@mail.ru' },
+                     { :username => 'fcbb95', :password => '123456', :email => 'fcbb95@mail.ru' }])
