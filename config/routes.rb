15d24bf1aab8946efe434016c4e5b196d17ad212
Eugene <q010@bk.ru>
2018-12-25 12:16:43 +0300

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   resources :technologies
+  get '/profile', to: 'users#profile'
   root 'technologies#index'
 end
