0f8c8ec67e942393ef32758d74f2ffbb62d75e87
Eugene <q010@bk.ru>
2018-12-26 23:25:45 +0300

    seeds with all references

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 #
+#
 Users = User.create([{ username: 'fcbb99', password: '123456', email: 'fcbb99@mail.ru', confirmed_at: '2018-12-31 15:32:10' },
                      { username: 'fcbb98', password: '123456', email: 'fcbb98@mail.ru', confirmed_at: '2018-12-31 15:32:10' },
                      { username: 'fcbb97', password: '123456', email: 'fcbb97@mail.ru' },
@@ -32,16 +33,37 @@ Answers1 = Answer.create([{answer_text: 'ans1'},
                           {answer_text: 'ans2'},
                           {answer_text: 'ans3'},
                           {answer_text: 'ans4'}])
-
 Answers2 = Answer.create([{answer_text: 'ans1'},
                           {answer_text: 'ans2'},
                           {answer_text: 'ans3'},
                           {answer_text: 'ans4'}])
 
+Answers3 = Answer.create([{answer_text: 'ans1'},
+                          {answer_text: 'ans2'},
+                          {answer_text: 'ans3'},
+                          {answer_text: 'ans4'}])
+
+Answers4 = Answer.create([{answer_text: 'ans1'},
+                          {answer_text: 'ans2'},
+                          {answer_text: 'ans3'},
+                          {answer_text: 'ans4'}])
+
 Questions = Tests[0].questions.create([{question_text: 'task1'},
-                                       {question_text: 'task2'}])
+                                       {question_text: 'task2'},
+                                       {question_text: 'task3'},
+                                       {question_text: 'task4'}])
 
 Questions[0].answers = Answers1
-
+Questions[0].right_answer = Answers1[1].id
+Questions[0].save
 Questions[1].answers = Answers2
+Questions[1].right_answer = Answers2[1].id
+Questions[1].save
+Questions[2].answers = Answers3
+Questions[2].right_answer = Answers3[2].id
+Questions[2].save
+Questions[3].answers = Answers4
+Questions[3].right_answer = Answers4[3].id
+Questions[3].save
+
 
