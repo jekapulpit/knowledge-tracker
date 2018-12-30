8fecb5310ae846bb3f533cdcfd9cadf2de620212
Eugene <q010@bk.ru>
2018-12-30 13:37:30 +0300

    setting event listeners on each form

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,8 +1,13 @@
-document.addEventListener('ajax:success', function(event) {
-    var detail = event.detail;
-    var isPassed = detail[0]['success'];
-    if (isPassed)
-        alert('uraaa');
-    else
-        alert('oh nooo');
+document.addEventListener('DOMContentLoaded', function() {
+    forms = document.getElementsByClassName('question-form');
+    for (i = 0; i < forms.length; i++) {
+        forms[i].addEventListener('ajax:success', function(event) {
+            var detail = event.detail;
+            var isPassed = detail[0]['success'];
+            if (isPassed)
+                alert('uraaa');
+            else
+                alert('oh nooo');
+        });
+    }
 });
