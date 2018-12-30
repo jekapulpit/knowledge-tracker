ece8351ec8ad7b2331818d253f6650149a20dc05
Eugene <q010@bk.ru>
2018-12-31 02:49:27 +0300

    rubocop changes

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module KnowledgeTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     el_config = {
-        host: 'http://localhost:9200/',
-        transport_options: {
-            request: { timeout: 5 }
-        },
+      host: 'http://localhost:9200/',
+      transport_options: {
+        request: { timeout: 5 }
+      }
     }
     config.load_defaults 5.1
     config.assets.enabled = true
