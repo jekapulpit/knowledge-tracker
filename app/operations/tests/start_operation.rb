464bc6c3ceacc9e35043b17c3ac106da9eba7215
Eugene <q010@bk.ru>
2018-12-30 16:58:54 +0300

    removing instance variables

--- app/operations/tests/start_operation.rb
+++ app/operations/tests/start_operation.rb
@@ -1,16 +1,18 @@
 module Tests
   class StartOperation
+    attr_reader :test, :user
+
     def initialize(test, user)
       @test = test
       @user = user
     end
 
     def call
-      @user.tests << @test unless @test.in? @user.tests
-      @user.technologies << @test.technology unless @test.technology.in? @user.technologies
-      @test_result = @user.test_results.find_by(test_id: @test.id)
-      @test_result.result = 0
-      @test_result.save
+      user.tests << test unless test.in? user.tests
+      user.technologies << test.technology unless test.technology.in? user.technologies
+      test_result = user.test_results.find_by(test_id: test.id)
+      test_result.result = 0
+      test_result.save
     end
   end
 end
