aa626898b55c8597e1e3b4a9d506c55803b1cb3d
Eugene <q010@bk.ru>
2019-01-04 22:36:24 +0300

    replacing sort options to helper

--- app/helpers/technologies_helper.rb
+++ app/helpers/technologies_helper.rb
@@ -1,2 +1,10 @@
 module TechnologiesHelper
+  def get_sort_options
+    [
+        ['most viewed', 'views-desc'],
+        ['less viewed', 'views'],
+        ['newest', 'created_at'],
+        ['oldest', 'created_at-desc'],
+    ]
+  end
 end
