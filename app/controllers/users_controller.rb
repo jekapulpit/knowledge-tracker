25d81ac5653908fa66cb9a5b48aa5379e9854520
Eugene <q010@bk.ru>
2019-01-07 17:11:35 +0300

    refactoring #update_progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,17 @@ class UsersController < ApplicationController
   def update_progress(technology)
     passed_tests = 0
     technology.tests.each do |test|
-      max_result = current_user.test_results.find_by(test_id: test.id, result: 6)
+      max_result = current_user
+                       .test_results
+                       .find_by(test_id: test.id, result: 10)
       if max_result
         passed_tests = passed_tests + 1
       end
     end
-    user_technology = current_user.technologies_users.find_by(technology_id: technology.id)
-    TechnologiesUser
-        .update(user_technology.id, :progress => passed_tests*100/technology.tests.count)
+    current_user
+        .technologies_users
+        .find_by(technology_id: technology.id)
+        .update(progress: passed_tests*100/technology.tests.count)
   end
 
   def user_params
