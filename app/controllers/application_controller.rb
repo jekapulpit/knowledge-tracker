fb434045b24f432249044ba21515e484b8d29b47
Eugene <q010@bk.ru>
2019-01-08 19:52:35 +0300

    setting layouts in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   protect_from_forgery with: :exception
@@ -18,5 +19,15 @@ class ApplicationController < ActionController::Base
                                       keys: %i[username email password password_confirmation])
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: %i[username email password password_confirmation current_password])
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      'auth'
+    else
+      'application'
+    end
   end
 end
