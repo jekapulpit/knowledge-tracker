28ed1110230f8f46ff4c2aca0f35d74459e9b4d0
Eugene <q010@bk.ru>
2019-01-08 18:11:15 +0300

    stylesheet for test

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    forms = document.getElementsByClassName('question-form');
+    var forms = document.getElementsByClassName('question-form');
     for (i = 0; i < forms.length; i++) {
         forms[i].addEventListener('ajax:success', function(event) {
             var detail = event.detail;
             var isPassed = detail[0]['success'];
-            if (isPassed)
-                alert('uraaa');
-            else
-                alert('oh nooo');
+            var button = event.target.getElementsByClassName('submit-btn')[0];
+
+            if (isPassed) {
+                button.setAttribute('class', 'submit-btn success-btn');
+            }
+            else {
+                button.setAttribute('class', 'submit-btn fail-btn');
+            }
+
+            event.target.disabled = true;
         });
     }
 });
