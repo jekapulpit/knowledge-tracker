0f26a03fbfc968ec6ac9583c2c8f4b48f2733ed9
yauhen <yauhen@wse-181-71b.local>
2019-03-14 17:16:18 +0300

    rendered tests and the rest attributes

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,13 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :users, through: :test_results
   belongs_to :technology
+
+  def with_passed_by_user(user)
+    attributes
+        .merge(passed: passed_by_user?(user))
+  end
+
+  def passed_by_user?(user)
+    user.test_results.find_by(test: self, result: self.questions.count)
+  end
 end
