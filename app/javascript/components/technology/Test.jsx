6b1386108747f14c18d11b2626deaac17d85005c
yauhen <yauhen@wse-181-71b.local>
2019-03-20 14:39:34 +0300

    added front-end implemention of test editing

--- app/javascript/components/technology/Test.jsx
+++ app/javascript/components/technology/Test.jsx
@@ -7,18 +7,32 @@ class Test extends React.Component {
       editable: false,
       test: props.test
     };
+    this.handleUpdate = this.handleUpdate.bind(this)
   }
 
+  handleUpdate = (test) => {
+    this.setState({
+      editable: !this.state.editable
+    });
+  };
+
   render() {
-    let isPassed = this.state.test.passed ? (<div className='test-status'> passed</div>) : null;
+    let status = this.state.test.passed ? (<div className='test-status passed'>v</div>) : (<div className='test-status not-passed'>x</div>);
+    let buttonVal = this.state.editable ? 'save' : 'edit';
+    let title = this.state.editable ? (<input type='text' ref={input => this.title = input} defaultValue={this.state.test.title}/>) :
+        (<a href={'/technologies/' + this.props.technologyId + '/tests/' + this.state.test.id}>
+          {this.state.test.title}
+        </a>);
+    let discription = this.state.editable ? (<textarea ref={input => this.discription = input}>{this.state.test.discription}</textarea>) :
+        (<p>{this.state.test.discription}</p>);
     return(
         <div className="theme-block">
           <div className="theme">
-            <div className="number">1</div>
-            <a href={'/technologies/' + this.props.technologyId + '/tests/' + this.state.test.id}>{this.state.test.title}</a>
-            <p>{this.state.test.discription}</p>
+            <div className="number">{status}</div>
+            {title}
+            {discription}
           </div>
-          {isPassed}
+          <div className="edit-button"><button onClick={(e) => this.handleUpdate(this.state.test)}>{buttonVal}</button></div>
         </div>
     )
   }
