9c54936fce55467ccfa12b1e7d084842c4f22125
yauhen <yauhen@wse-181-71b.local>
2019-03-28 12:21:47 +0300

    changed default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_091639) do
+ActiveRecord::Schema.define(version: 2019_03_28_092046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
