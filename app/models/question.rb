fc70cc0e40c10e678e434e2845e62d0948a6c178
yauhen <yauhen@wse-181-71b.local>
2019-03-28 13:17:47 +0300

    added different types of questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy, inverse_of: :question
+  has_one_attached :picture
   belongs_to :test
+  enum type: [ :checkbox, :radio, :text ]
   accepts_nested_attributes_for :answers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_092046) do
+ActiveRecord::Schema.define(version: 2019_03_28_100522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_03_28_092046) do
     t.bigint "answers_id"
     t.bigint "test_id"
     t.integer "right_answer"
+    t.integer "type"
     t.index ["answers_id"], name: "index_questions_on_answers_id"
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
