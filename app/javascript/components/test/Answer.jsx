d2c375e119cbbcd81d83fff5d1a4aea2a00d7c8c
yauhen <yauhen@wse-181-71b.local>
2019-04-02 15:32:33 +0300

    rendered all answers inside a question

---
+++ app/javascript/components/test/Answer.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Answer extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          editable: false,
+          answer: props.answer
+      };
+  }
+
+  render () {
+    return (
+      <span className="radio">
+        <label htmlFor={"question_right_answer_" + this.state.answer.id}>
+          <input name={"question["+ this.props.question_id +"][right_answer]"}
+                 type="radio"
+                 value={this.state.answer.id}
+                 id={"question_right_answer_" + this.state.answer.id}
+                 onChange={this.props.handleOptionChange}
+                 defaultChecked={this.props.selected === this.state.answer.id}/>
+            {this.state.answer.answer_text}
+        </label>
+      </span>
+    );
+  }
+}
+
+export default Answer
--- app/javascript/components/test/Question.jsx
+++ app/javascript/components/test/Question.jsx
@@ -1,14 +1,18 @@
 import React from "react"
 import Carousel from 'react-bootstrap/Carousel'
-
 import PropTypes from "prop-types"
+import Answer from "./Answer";
+
 class Question extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       question: props.question,
-      answers: []
+      answers: [],
+      selectedAnswer: props.question.right_answer,
+      available: true
     };
+    this.handleOptionChange = this.handleOptionChange.bind(this)
   }
 
   handleUpdate = () => {
@@ -34,25 +38,32 @@ class Question extends React.Component {
         })
   };
 
+  handleOptionChange = (changeEvent) => {
+      this.setState({
+          selectedAnswer: changeEvent.target.value
+      });
+  };
+
+  handleBlock = () => {
+      this.setState({
+          available: false
+      });
+  }
+
   render () {
+    let answers = this.state.question.answers.map((answer) => {
+      return(<Answer handleOptionChange={this.handleOptionChange}
+                     question_id={this.state.question.id}
+                     answer={answer}
+                     selected={this.state.selectedAnswer}/>)
+    });
     return (
             <div className="question-block">
               <form className="question-form">
                 <input type='text' ref={input => this.question_text = input} defaultValue={this.state.question.question_text}/>
                 <input type='text' ref={input => this.test_id = input} defaultValue={this.state.question.test_id} style={{ display: 'none' }} />
                 <div className="custom-radios">
-                <span className="radio">
-                  <label htmlFor={"question_right_answer_1_" + this.state.question.id}>
-                    <input ref={input => this.right_answer = input} type="radio" value="1" id={"question_right_answer_1_" + this.state.question.id}/>
-                    stubbb
-                  </label>
-                </span>
-                  <span className="radio">
-                  <label htmlFor={"question_right_answer_1_" + this.state.question.id}>
-                    <input ref={input => this.right_answer = input} type="radio" value="2" id={"question_right_answer_2_" + this.state.question.id}/>
-                    stubbb
-                  </label>
-                </span>
+                  {answers}
                 </div>
               </form>
             </div>
