8edd07fd28b82bf3eeda09a5438afef9cee50631
yauhen <yauhen@wse-181-71b.local>
2019-04-02 19:40:09 +0300

    added stub for update answer feachure

--- app/javascript/components/test/Answer.jsx
+++ app/javascript/components/test/Answer.jsx
@@ -7,23 +7,33 @@ class Answer extends React.Component {
       this.state = {
           editable: false,
           answer: props.answer,
-          status: props.status
+          created: props.created
       };
   }
 
+  handleEdit = () => {
+    this.setState({
+        created: !this.state.created
+    })
+  };
+
   render () {
-    let actionButton = this.state.status ? (<button>edit</button>) : (<button>save</button>);
+    let input = this.state.created ?
+        (<label htmlFor={"question_right_answer_" + this.state.answer.id}>
+        <input name={"question["+ this.props.question_id +"][right_answer]"}
+              type="radio"
+              value={this.state.answer.id}
+              id={"question_right_answer_" + this.state.answer.id}
+              onChange={this.props.handleOptionChange}
+              defaultChecked={this.props.selected === this.state.answer.id}/>
+            {this.state.answer.answer_text}
+        </label>)
+        :
+        (<input ref={input => this.answer_text = input} defaultValue={this.state.answer.answer_text}/>);
     return (
       <span className="radio">
-        <label htmlFor={"question_right_answer_" + this.state.answer.id}>
-          <input name={"question["+ this.props.question_id +"][right_answer]"}
-                 type="radio"
-                 value={this.state.answer.id}
-                 id={"question_right_answer_" + this.state.answer.id}
-                 onChange={this.props.handleOptionChange}
-                 defaultChecked={this.props.selected === this.state.answer.id}/>
-            {this.state.answer.answer_text}
-            </label>
+          {input}
+          <p onClick={() => this.handleEdit()}> {this.state.created ? 'edit' : 'save'} </p>
       </span>
   );
   }
